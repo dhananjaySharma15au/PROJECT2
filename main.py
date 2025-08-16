a02eba2f14f10e3aa2e39be2215d24470d6921a1
dhananjay sharma <47531604+dhananjaySharma15au@users.noreply.github.com>
2025-08-16 10:26:53 +0000

    number guesser

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import random
 randomNum = random.randrange(-1,101)
-print(randomNum)
+print(randomNum,"\n")
 guess = counter = 0
 while(guess != -1):
- guess = input("Guess thr number :")
+ guess = input("Guess thr number : ")
+ if not guess.isdigit():
+         print("invalid input\n\nNOTE :- not be considered as a guess\n")
+         print("please enter a number\n")
+         continue
  if int(guess) == randomNum:
-         print("you got it right in your try number : "+ str(counter))
+         print("you got it right in your try number : ",str(counter))
          break
  elif int(guess) < randomNum:
          counter += 1
-         print("you got it less then the correct number")
-         print("try again")
+         print("you got it less then the correct number\n")
+         print("try again\n")
  else :
          counter += 1
-         print("you got it more then the correct number")
-         print("try again")
+         print("you got it more then the correct number\n")
+         print("try again\n")
